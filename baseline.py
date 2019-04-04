bfca695f42c313b4c10838a1e611d8d1599e5dc4
dusmart <dusmart@qq.com>
2019-04-04 16:04:14 +0800

    improve evaluation speed

--- baseline.py
+++ baseline.py
@@ -2,6 +2,7 @@ import pickle
 from tqdm import tqdm
 from parameters import *
 import time
+from evaluation import evaluation
 
 def exeTime(func):
     def newFunc(*args, **args2):
@@ -19,8 +20,8 @@ with open(arg2id_path, 'rb') as fin:
     arg2id = pickle.load(fin)
 
 
-init_deprel_dict = {deprel:set() for deprel in deprel2id.keys()}
-init_arg_dict = {arg:set() for arg in arg2id.keys()}
+init_deprel_dict = {deprel:[] for deprel in deprel2id.keys()}
+init_arg_dict = {arg:[] for arg in arg2id.keys()}
 
     
 
@@ -44,8 +45,8 @@ def get_label(flattened_data_path):
                         deprel = word_info[12]
                         arg = word_info[14]
                         idx = (word_info[0],word_info[1],word_info[4])
-                        groundtruths[predicate][arg].add(idx)
-                        predicts[predicate][deprel].add(idx)
+                        groundtruths[predicate][arg].append(idx)
+                        predicts[predicate][deprel].append(idx)
             sentence = []
             predicate = None
         else:
@@ -68,9 +69,17 @@ def get_label(flattened_data_path):
 @exeTime
 def main():
     truths, predicts = get_label(flattened_train_data_path)
-    from evaluation import fast_eval_f1, eval_f1
-    pre, coll, _, _ = fast_eval_f1(truths, predicts)
-    print(pre, coll)
+    pre, coll, f1 = evaluation(truths, predicts)
+    print(pre, coll, f1)
+    # set_truths, set_predicts = dict(), dict()
+    # for word in tqdm(truths.keys()):
+    #     set_truths[word] = dict()
+    #     set_predicts[word] = dict()
+    #     for truth_label in truths[word].keys():
+    #         set_truths[word][truth_label] = set(truths[word][truth_label])
+    #     for pre_label in predicts[word].keys():
+    #         set_predicts[word][pre_label] = set(predicts[word][pre_label])
+    # pre, coll, _, _ = eval_f1(set_truths, set_predicts)
 
     truths_combine = {'combine': init_arg_dict.copy()}
     predicts_combine = {'combine': init_deprel_dict.copy()}
@@ -81,8 +90,8 @@ def main():
         for key in predicts[word].keys():
             predicts_combine['combine'][key] = predicts_combine['combine'][key] | predicts[word][key]
     
-    pre, coll, _, _ = fast_eval_f1(truths_combine, predicts_combine)
-    print(pre, coll)
+    pre, coll, f1 = evaluation(truths_combine, predicts_combine)
+    print(pre, coll, f1)
 
 if __name__ == "__main__":
     main()
--- split_merge.py
+++ split_merge.py
@@ -1,32 +1,76 @@
 import pickle
 from tqdm import tqdm
 from parameters import *
+import numpy as np
+import scipy.spatial.distance
+from evaluation import evaluation
+from typing import *
+
 
 with open(deprel2id_path, 'rb') as fin:
     deprel2id = pickle.load(fin)
 with open(arg2id_path, 'rb') as fin:
     arg2id = pickle.load(fin)
-with open(preposition2id_path, 'rb') as fin:
-    preposition2id = pickle.load(fin)
+with open(arghead2id_path, 'rb') as fin:
+    arghead2id = pickle.load(fin)
+with open(pos2id_path, 'rb') as fin:
+    pos2id = pickle.load(fin)
 
-prepositions = set(preposition2id.keys())
 
-init_key_dict = {(deprel, verbvoice, rela_position, preposition) :set() 
+init_key_dict = {(deprel, verbvoice, rela_position) :[]
     for deprel in deprel2id.keys()
-    for verbvoice in ['a', 'p'] 
-    for rela_position in ['l', 'r']
-    for preposition in prepositions}
-init_arg_dict = {arg:set() for arg in arg2id.keys()}
+    for verbvoice in ['a', 'p']
+    for rela_position in ['l', 'r']}
+init_arg_dict = {arg:[] for arg in arg2id.keys()}
+
+
+class Cluster:
+    BETA = 0
+    GAMMA = 0
+    def __init__(self, data: List[Tuple[str,str,str]]) -> None:
+        self.data = data
+        self.lex = np.array([0] * len(arghead2id))
+        self.pos = np.array([0] * len(pos2id))
+    def pos_sim(self, other):
+        return 1 - scipy.spatial.distance.cosine(self.pos, other.pos)
+    def lex_sim(self, other):
+        return 1 - scipy.spatial.distance.cosine(self.lex, other.lex)
+    def cons_sim(self, other):
+        viol = 0
+        i, j = 0, 0
+        while i < len(self.data) and j < len(other.data):
+            if self.data[i][0] == other.data[i][0] and self.data[i][1] == other.data[i][1]:
+                viol += 1
+                i += 1
+                j += 1
+            elif self.data[i] < other.data[j]:
+                i += 1
+            else:
+                j += 1
+        return 1 - 2 * viol / (len(self.data) + len(other.data))
+    def score(self, other, beta, gamma):
+        """ calculate the similarity between two clusters
+        """
+        if self.pos_sim(other) < Cluster.BETA:
+            return 0
+        elif self.cons_sim(other) < Cluster.GAMMA:
+            return 0
+        return self.lex_sim(other)
+    def __iadd__(self, other):
+        self.data.extend(other.data)
+        self.data.sort()
+        self.lex += other.lex
+        self.pos += other.pos
 
 
 
-def get_label(flattened_data_path):
+
+def split_phase(flattened_data_path):
     groundtruths = dict()
     predicts = dict()
     sentences = []
     with open(flattened_data_path, 'r') as fin:
         sentences = fin.readlines()
-
 
     sentence = []
     predicate = None
@@ -39,12 +83,11 @@ def get_label(flattened_data_path):
                     if word_info[-1] != '_':
                         rela_position = 'r' if int(word_info[4]) > predicate_id else 'l'
                         verbvoice = 'p' if sentence[predicate_id-1][8] == 'VBN' else 'a'
-                        preposition = '<PAD>' if word_info[8] != 'IN' or word_info[8] not in prepositions else word_info[6]
                         deprel = word_info[12]
                         arg = word_info[14]
                         idx = (word_info[0],word_info[1],word_info[4])
-                        groundtruths[predicate][arg].add(idx)
-                        predicts[predicate][(deprel,verbvoice,rela_position,preposition)].add(idx)
+                        groundtruths[predicate][arg].append(idx)
+                        predicts[predicate][(deprel,verbvoice,rela_position)].append(idx)
             sentence = []
             predicate = None
             predicate_id = -1
@@ -56,36 +99,26 @@ def get_label(flattened_data_path):
                     groundtruths[predicate] = init_arg_dict.copy()
                     predicts[predicate] = init_key_dict.copy()
             sentence.append(word_info)
-    if len(sentence) != 0 and predicate is not None:
-        for word_info in sentence:
-            if word_info[-1] != '_':
-                        rela_position = 'r' if int(word_info[4]) > predicate_id else 'l'
-                        verbvoice = 'p' if sentence[predicate_id-1][8] == 'VBN' else 'a'
-                        preposition = '<PAD>' if word_info[8] != 'IN' else word_info[6]
-                        deprel = word_info[12]
-                        arg = word_info[14]
-                        idx = (word_info[0],word_info[1],word_info[4])
-                        groundtruths[predicate][arg].add(idx)
-                        predicts[predicate][(deprel,verbvoice,rela_position,preposition)].add(idx)
+    assert(len(sentence)==0 and predicate is None)
     return groundtruths, predicts
 
-def main():
-    truths, predicts = get_label(flattened_test_data_path)
-    from evaluation import fast_eval_f1, eval_f1
-    pre, coll, _, _ = fast_eval_f1(truths, predicts)
-    print(pre, coll)
 
-    truths_combine = {'combine': init_arg_dict.copy()}
-    predicts_combine = {'combine': init_key_dict.copy()}
-    for word in truths:
-        for key in truths[word].keys():
-            truths_combine['combine'][key] = truths_combine['combine'][key] | truths[word][key]
-    for word in predicts:
-        for key in predicts[word].keys():
-            predicts_combine['combine'][key] = predicts_combine['combine'][key] | predicts[word][key]
-    
-    pre, coll, _, _ = fast_eval_f1(truths_combine, predicts_combine)
-    print(pre, coll)
+def main():
+    truths, predicts = split_phase(flattened_test_data_path)
+    pre, coll, f1 = evaluation(truths, predicts)
+    print(pre, coll, f1)
+
+    # 1. combine all those verb's clusters together and calculate new result
+    # truths_combine = {'combine': init_arg_dict.copy()}
+    # predicts_combine = {'combine': init_key_dict.copy()}
+    # for word in truths:
+    #     for key in truths[word].keys():
+    #         truths_combine['combine'][key] = truths_combine['combine'][key] | truths[word][key]
+    # for word in predicts:
+    #     for key in predicts[word].keys():
+    #         predicts_combine['combine'][key] = predicts_combine['combine'][key] | predicts[word][key]
+    # pre, coll, _, _ = eval_f1(truths_combine, predicts_combine)
+    # print(pre, coll)
 
 if __name__ == "__main__":
     main()
