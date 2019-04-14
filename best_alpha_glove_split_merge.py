e8cf434f3db28e219ff63a0f88c435b7d00e1edc
dusmart <dusmart@qq.com>
2019-04-14 14:19:37 +0800

    fix the search range to 0.8to1.0

--- best_alpha_glove_split_merge.py
+++ best_alpha_glove_split_merge.py
@@ -4,10 +4,10 @@ from glove_split_merge import *
 def got_best_alpha():
     Cluster.GAMMA = 0.95
     best_pre, best_coll, best_f1, best_alpha = 0, 0, 0, 0
-    truths, predicts = split_phase(flattened_sample_data_path)
+    truths, predicts = split_phase(flattened_train_data_path)
     split_pre, split_coll, split_f1 = evaluation(truths, predicts)
     print("====split====", split_pre, split_coll, split_f1)
-    for alpha in np.arange(0.65, 1, 0.05):
+    for alpha in np.arange(0.8, 1, 0.02):
         final_pre = merge_phases(deepcopy(predicts), alpha)
         pre, coll, f1 = evaluation(truths, final_pre)
         print("====merge {}====".format(alpha), pre, coll, f1)
